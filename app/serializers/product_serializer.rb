72338b3dfb6eb2fe1c74b8962065e6259c10fecd
Taylor Williams <taylor@thewilliams.net>
2019-03-04 19:59:01 +0000

    Automatically backed up by Learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,10 +1,10 @@
 class ProductSerializer
   def self.serialize(product)
     pro = '{'
-    pro += '"name": ' + product.name + ', '
+    pro += '"name": "' + product.name + '", '
     pro += '"price": ' + product.price.to_s + ', '
     pro += '"inventory": ' + product.inventory.to_s + ', '
-    pro += '"description": ' + product.description
+    pro += '"description": "' + product.description + '"'
     pro += '}'
   end
 end
